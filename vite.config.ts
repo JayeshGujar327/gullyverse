da78d805f97d50d5b37d81e964666f099fb1a633
Jayesh Gujar <121622212+JayeshGujar327@users.noreply.github.com>
2026-08-20 16:34:15 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  base: '/gullyverse/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    server: {
-    },
-  };
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 });
-
